e5f6a7002f29e12e15b6fa7fce582b91a6ec34bc
Devjallow <bjallow000@outlook.com>
2022-06-02 16:15:39 -0700

    program that prins a proper grammer

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - return the interger 0 from the main function
+ *
+ * Description:"with proper grammer, but the outcome is a piece of art,"
+ *
+ * Return> 0 from the main function
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
