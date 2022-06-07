f713cb54d80edaf5c45b79ac6197820e3addb9a8
Devjallow <bjallow000@outlook.com>
2022-06-06 21:02:57 -0700

    prints all possible combo of two digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - main - two pairs of number
+ *
+ * Description: Number do not repeat
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 99; j++)
+	{
+		for (i = j; i <= 99; i++)
+		{
+			if (i != 10)
+			{
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+			}
+			if (j * 100 + i != 9899)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
