8b0f23c0a01fd01b6f047dbf17a59b804eb893cd
Devjallow <bjallow000@outlook.com>
2022-06-07 19:01:38 -0700

    print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,16 @@
 
 void print_alphabe_x10(void)
 {
-	int b = 0;
-	while (b < 10)
-	{
-		char a = 'a';
-	while (a <= 'z')
-	{
-		_putchar(a);
-		a++;
+int b = 0;
+while (b < 10)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
 
-	}
-	_putchar('\n');
-	b++;
+}
+_putchar('\n');
+b++;
 }
