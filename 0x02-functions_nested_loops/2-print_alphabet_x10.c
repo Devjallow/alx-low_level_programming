219dcb28e7a6f43707f5b8f5ebe99288235dbeb0
Devjallow <bjallow000@outlook.com>
2022-06-07 18:50:06 -0700

    functions and their prototype

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - printsthe aphae alphabets 10 times
+ *
+ * Return: return 0
+ */
+
+void print_alphabe_x10(void)
+{
+	int b = 0;
+	while (b < 10)
+	{
+		char a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+
+	}
+	_putchar('\n');
+	b++;
+}
