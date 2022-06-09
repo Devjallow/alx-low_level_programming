325fd27ead1c3f89615d342769cd253677ac6562
Devjallow <bjallow000@outlook.com>
2022-06-08 19:56:04 -0700

    sums up natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - sus multiples of 3 and 5
+ *
+ * Description: mutiples between 0 and 1024
+ *
+ * Return: Alwyas return 0.
+ */
+
+int main(void)
+{
+	int start_num;
+	int end_num;
+	int total;
+
+	for (start_num = 0; start_num < end_num; start_num++)
+	{
+		if ((start_num % 3 == 0) || (start_num % 5 == 0)
+		{
+		total = tolat + start_num;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("/d", total);
+	printf("\n");
+
+	return (0);
+}
