f62e3720792e1fd404c1738aada7bf2581c8bf8e
Devjallow <bjallow000@outlook.com>
2022-06-08 19:14:35 -0700

    print the 9 timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 table
+ *
+ * Return: Alwyas return 0.
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j == 0)
+			{
+				_putchar(48);
+				continue;
+			}
+			product = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (product >= 10)
+			{
+				_putchar(product / 10 + 48);
+				_putchar(product % 10 + 48);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(prodcut + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
