1063bd896b80d0c622b99e9f4ee7e0544d8eef8d
Devjallow <bjallow000@outlook.com>
2022-06-09 06:27:50 -0700

    check for uppercase character

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int _isupper(int c) - checks for upper characters
+ *
+ * Return: Always return 0 or 1.
+ */
+
+int _isiupper(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,4 @@
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+voi print_numbers(voi)
