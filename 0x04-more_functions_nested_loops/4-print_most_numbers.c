91fec1cc273ed854f7afaa09f3109155460cdceb
Devjallow <bjallow000@outlook.com>
2022-06-10 19:34:37 -0700

    prints the numbers from 0 - 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 	int ch;
 
-	for (cha = 48; ch < 58; ch++)
+	for (ch = 48; ch < 58; ch++)
 	{
-		if (ch != 50 && cha != 52)
+		if (ch != 50 && ch != 52)
 			_putchar(ch);
 	}
 	_putchar('\n');
