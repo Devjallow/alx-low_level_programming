c6a26dec12eca4686561885d07f1f16584f00bd8
Devjallow <bjallow000@outlook.com>
2022-06-12 18:05:12 -0700

    prints x10 the numbers from 0 - 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,9 +3,10 @@
 /**
  * more_numbers - more numbers
  * 
- * Description: prints 10 times the numbers.
- * Return: no return
+ * Description: prints 10 times the numbers from 0 to 14.
+ * Return: Always (0)
  */
+
 void more_numbers(void)
 {
 	int n, i;
