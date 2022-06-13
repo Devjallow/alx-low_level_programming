d036cb89a42ee068f84a4b7794b008142a57a3b2
Devjallow <bjallow000@outlook.com>
2022-06-12 18:44:11 -0700

    prints a straightline

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "mian.h"
+
+/**
+ * print_line - prints straight line
+ * @n: Times straight line is printed.
+ * Return: no return.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n);
+}
