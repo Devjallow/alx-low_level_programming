4c54765ea8038f4b352870d826d2f351c3e07e16
Devjallow <bjallow000@outlook.com>
2022-11-02 19:10:07 -0700

    fixed the error

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _put_recursion - prints a string followed by a new line
+ * _puts_recursion - prints a string followed by a new line
  * @s: The string to be printned.
  * return: Always return success.
  */
-void _put_recursion(char *s)
+
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-		return;
+	if (*s)
 	{
-		_put_recursion(s + 1);
 		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
