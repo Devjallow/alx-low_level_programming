329b4f4c99680e43f604cbe0e30de1cf236588e7
Devjallow <bjallow000@outlook.com>
2022-11-05 07:32:25 -0700

    task completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program.
+ * @argc: print the number of argument
+ * @argv: print the array of characters
+ *  return: Always 0.
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
