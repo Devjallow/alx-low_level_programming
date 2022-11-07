71a3d026530fb1c5f15198cc60b7c255aea492b7
Devjallow <bjallow000@outlook.com>
2022-11-07 05:55:00 -0800

    fixed error

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,16 +5,20 @@
  * main - prints index of argument pass on the terminal
  * @argc: prints index of argument.
  * @argv: prints arrays of argument
- * return: always return success -.
+ * Return: always return 0.
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
 	{
-		printf("%d\n", i);
+		for (i = 0; *argv; i++, argv++)
+			;
+			printf("%d\n", i - 1);
 	}
-	return(0);
+	return (0);
 }
