763415a57b7bd9af0528a762e4bedbe2e714c602
Devjallow <bjallow000@outlook.com>
2022-11-14 07:08:21 -0800

    print program name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name of the program
+ * @argc: Prints number of argument
+ * @argv: Prints argument name
+ *
+ * Return: Always success.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
