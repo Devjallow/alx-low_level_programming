628bba0444391c769b1bde8361123584160bd46e
Devjallow <bjallow000@outlook.com>
2022-12-20 08:56:00 -0800

    error fixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,17 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	static int i;
-
-	i = 0;
-
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		i++;
-		_strlen_recursion(++s);
+		return (0);
 	}
 	else
 	{
-		return i;
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
